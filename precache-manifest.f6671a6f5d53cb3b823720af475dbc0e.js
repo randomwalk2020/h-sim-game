058d614243ad85136a17ea5c7abcd169c623369e
randomwalk2020 <64976869+randomwalk2020@users.noreply.github.com>
2020-05-07 13:01:36 -0400

    First upload work in progress

---
+++ precache-manifest.f6671a6f5d53cb3b823720af475dbc0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37cc10dcd81ca6874939dbf5b2161dbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8e8f7e59c8190203c73",
+    "url": "/static/css/main.49d77b5b.chunk.css"
+  },
+  {
+    "revision": "24a5a46c01d28de721f9",
+    "url": "/static/js/2.e8421d65.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e8421d65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8e8f7e59c8190203c73",
+    "url": "/static/js/main.93089c91.chunk.js"
+  },
+  {
+    "revision": "1cf0ca722e9401d68519",
+    "url": "/static/js/runtime-main.27ce9b4b.js"
+  }
+]);
